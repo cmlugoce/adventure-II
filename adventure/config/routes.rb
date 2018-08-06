f2d713968e5dabd5aad1ea9a5bd3bed0b2588535
cmlugoce <cmlugoce@syr.edu>
2018-08-06 14:55:35 -0700

    add main routes

--- adventure/config/routes.rb
+++ adventure/config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'parks/index'
-  get 'parks/show'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'trails/new'
-  get 'trails/show'
-  get 'trails/create'
-  get 'trails/edit'
-  get 'trails/update'
-  get 'users/new'
-  get 'users/show'
-  get 'users/create'
+  
+ root 'welcome#index'
+
+ resources :users, only: [:new, :create, :show] do 
+  resources :trails
+
+ end 
+
+ resources :parks, only: [:index, :show]
+
+ get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

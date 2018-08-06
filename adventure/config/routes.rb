21399a7433f2b95b88db6b7397e4ec92eeea3a00
cmlugoce <cmlugoce@syr.edu>
2018-08-06 14:25:27 -0700

    generate controllers and views

--- adventure/config/routes.rb
+++ adventure/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'parks/index'
+  get 'parks/show'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'trails/new'
+  get 'trails/show'
+  get 'trails/create'
+  get 'trails/edit'
+  get 'trails/update'
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
